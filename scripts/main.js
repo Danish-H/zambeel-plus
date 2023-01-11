247b118aab3b5a30d59252368e3f367369242553
Danish Humair <121830048+Danish-H@users.noreply.github.com>
2023-01-12 04:27:07 +0500

    Major restructuring, better tables, all-frame script, :monkey:

---
+++ scripts/main.js
@@ -0,0 +1,63 @@
+function updateTheme() {
+    if (localStorage.getItem("zpDarkMode") == "true") {
+        document.body.classList.add("dark");
+    } else {
+        document.body.classList.remove("dark");
+    }
+}
+
+function addBtnDark() {
+    if (document.getElementById("win0hdrdivPTLAYOUT_HEADER_GROUPBOX6")) {
+        document.getElementById("win0hdrdivPTLAYOUT_HEADER_GROUPBOX6").prepend(btnDark);
+    } else if (document.querySelector(".ps_header_bar .ps_actions_cont .ps_system_cont")) {
+        document.querySelector(".ps_header_bar .ps_actions_cont .ps_system_cont").prepend(btnDark);
+    } else if (document.getElementById("win354hdrdivPT_ACTION_CONT")) {
+        document.getElementById("win354hdrdivPT_ACTION_CONT").prepend(btnDark);
+    } else {
+        console.log("[!] Could not add dark mode button!")
+    }
+}
+
+function setAccent(colorA, colorB) {
+    var style = document.createElement('style');
+    style.textContent = 
+`:root:not(.psc_mode-hc) body .ps_header_bar-container:before,
+body #ptbr_header_container:before
+{ background: linear-gradient(to right, `+colorA+` 0%, `+colorB+` 100%) !important; }`;
+    document.head.appendChild(style);
+}
+
+
+console.log("[!] Zambeel+ is running");
+
+// Update theme
+updateTheme();
+
+// Create dark mode button
+btnDark = document.createElement("div");
+btnDark.classList.add("ps_box-button", "psc_image_only", "psc_toolaction-home", "ps_header_button", "psc_hide-BP2");
+btnDark.innerHTML =
+`<span class="ps-button-wrapper" title="Dark Mode"><a id="PT_HOME" class="ps-button" role="button" href="#" id="dark-mode-btn">
+<img style="filter: invert(1); max-height: 23px;" src="https://www.svgrepo.com/show/472736/moon.svg" class="ps-img" alt="Dark">
+</a></span>`;
+addBtnDark();
+
+// Change theme on click
+btnDark.onclick = () => {
+    document.body.classList.add("animated");
+    localStorage.setItem("zpDarkMode", !document.body.classList.contains("dark"));
+    updateTheme();
+}
+
+
+
+// Get roll number
+var roll = "";
+if (document.getElementById("pt_envinfo")) {
+    roll = document.getElementById("pt_envinfo").getAttribute("user");
+}
+
+// Just testing
+if (roll.endsWith("83")) {
+    setAccent("#FFB6C1", "#FF69B4");
+}
---
+++ scripts/marks.js
@@ -0,0 +1,113 @@
+function monkeyPatch() {
+    var script = document.createElement('script');
+    script.textContent = 
+`orig_submitAction_win0 = submitAction_win0;
+submitAction_win0 = function (form, id, event, sAjaxTrfUrl, bWarning, sScriptAfter, nTrfURLType) {
+  orig_submitAction_win0(form, id, event, sAjaxTrfUrl, bWarning, sScriptAfter, nTrfURLType);
+  console.log("frame reloaded!!!!!!!!!")
+}`;
+    script.setAttribute("language", "JavaScript");
+    document.head.appendChild(script);
+}
+
+function getURLParams(url) {
+    var qStart = url.indexOf("?") + 1;
+    var qEnd = url.indexOf("#") + 1 || url.length + 1;
+
+    var query = url.slice(qStart, qEnd - 1);
+    var pairs = query.replace(/\+/g, " ").split("&");
+
+    var parms = {}, i, n, v, nv;
+
+    if (query === url || query === "") return;
+
+    for (i=0; i<pairs.length; i++) {
+        nv = pairs[i].split("=", 2);
+        n = decodeURIComponent(nv[0]);
+        v = decodeURIComponent(nv[1]);
+
+        if (!parms.hasOwnProperty(n)) parms[n] = [];
+        parms[n].push(nv.length === 2 ? v : null);
+    }
+
+    return parms;
+}
+
+function printMeans(response) {
+    meansArr = response.means;
+    for (i=0; i<meansArr.length; i++) {
+        document.getElementById("DERIVED_LAM_EXPLANATION$"+ i).innerHTML += meansArr[i];
+    }
+    document.getElementById("DERIVED_LAM_LAM_SPECIAL_CHAR$17$").innerHTML += response.overall_mean;
+}
+
+window.onload = function () {
+    // User better tables
+    if (localStorage.getItem("zpBetterTables") == "on") {
+        document.body.classList.add("better-tables");
+    }
+
+    // Return if not marks page
+    if (!document.getElementById("win0divSTDNT_GRADE_DTL$0")) {
+        return;
+    }
+
+    // Monkey patch iframe loading
+    //monkeyPatch();
+
+    // Parse course information from URL
+    urlParams = getURLParams(window.location.search);
+    console.log("User: " + urlParams.EMPLID);
+    console.log("Term: " + urlParams.STRM);
+    console.log("Class: " + urlParams.CLASS_NBR);
+    marksArr = [];
+
+    // Element IDs for parsing marks
+    const ID_COMP = "ACTIVITY$";
+    const ID_CAT = "CATEGORY$";
+    const ID_MARK = "STDNT_GRADE_DTL_STUDENT_GRADE$";
+    const ID_MAX = "LAM_CLASS_ACTV_MARK_OUT_OF$";
+    const ID_OTHER = "DERIVED_LAM_EXPLANATION$";
+    const ID_OVERALL  = "STDNT_GRADE_HDR_GRADE_AVG_CURRENT";
+
+    for (i=0; document.getElementById(ID_COMP + i) != null; i++) {
+        if (document.getElementById(ID_MARK + i) != null) {
+            let mark = {
+                "component_no": i,
+                "component": document.getElementById(ID_COMP + i).innerHTML,
+                "mark": document.getElementById(ID_MARK + i).innerHTML,
+                "max_mark": document.getElementById(ID_MAX + i).innerHTML
+            };
+            marksArr.push(mark);
+            document.getElementById(ID_OTHER + i).innerHTML += "✔ Read by <b style='color: purple;'><i>Z+</i></b>";
+        }
+    }
+
+    if (document.getElementById(ID_OVERALL).innerHTML != null) {
+        marksArr.push({
+            "component_no": -1,
+            "component": "Overall",
+            "mark": document.getElementById(ID_OVERALL).innerHTML,
+            "max_mark": "100.00"
+        });
+    }
+
+    // JSON data for POST
+    let jsondata = JSON.stringify({
+        user: urlParams.EMPLID[0],
+        term: urlParams.STRM[0],
+        class: urlParams.CLASS_NBR[0],
+        marks: marksArr
+    });
+
+    chrome.runtime.sendMessage(
+        jsondata,
+        data => {
+            try {
+                printMeans(JSON.parse(data))
+            } catch(err) {
+                console.log("[!] Did not get a valid response from API!")
+            }
+        }
+    );
+}
